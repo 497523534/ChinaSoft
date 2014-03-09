65e4f38cfa00dfe0ff5e14c74e545514d0c1a13a
497523534 <xx0_0dd@sina.com>
2014-03-09 14:09:54 +0800

    ..

--- src/dao/JDBCdemo.java
+++ src/dao/JDBCdemo.java
@@ -4,32 +4,43 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCdemo {
 	private String driverClass="com.mysql.jdbc.Driver";
 	private String jdbcURL="jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8";
 	private String user="root";
 	private String pwd="";
-	private java.sql.PreparedStatement pstmt = null ;
+	//private java.sql.PreparedStatement pstmt = null ;
 	private Connection conn=null;
 	public void getConnection()throws Exception {
 			Class.forName(driverClass);
 			conn=DriverManager.getConnection(jdbcURL, user, pwd);	
-			System.out.println("yes");
+			//System.out.println("yes");
 	}
 	public boolean checkUser(String username,String userpassword){
 		    boolean Value=false;
 		    try {
-		    	String sql ="   select*from userinfo where username=? and  userpassword=? " ;
-				pstmt = conn.prepareStatement(sql);		
-				pstmt.setString(1, username);
-				pstmt.setString(2, userpassword);
-				ResultSet rs = pstmt.executeQuery();
-				while(rs.next()){
+		    	Statement stmt=conn.createStatement();
+		    	String sql =" select*from userinfo " ;
+				//pstmt = (PreparedStatement)conn.prepareStatement(sql);		
+				//pstmt.setString(1, username);
+				//pstmt.setString(2, userpassword);
+				ResultSet rs = stmt.executeQuery(sql);
+				while(true){
+				  if(rs.next()){
+					  if(rs.getString(1).equals(username)&&rs.getString(2).equals(userpassword))
+					  {
+					  Value=true;
+					  break;
+					  }
+				  }
+				  else {
+					break;
+				}
 				  
-				   Value=true;
-						 break;
-				   }
+				
+			}
 		    } catch (SQLException e) {
 				e.printStackTrace();
 			}
--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,14 +1,11 @@
 package servlet;
 
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import dao.JDBCdemo;
 
 
@@ -17,16 +14,14 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     public LoginServlet() {  
     }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
 		request.setCharacterEncoding("utf-8");
 		String username=request.getParameter("uname");
 		String userpassword=request.getParameter("upassword");
-		System.out.println(username+"  "+userpassword);
+		//System.out.println(username+"  "+userpassword);
 		//String result="";
 		/*if((username== "")||(username==null)||(username.length()>20)){
 			   try{
